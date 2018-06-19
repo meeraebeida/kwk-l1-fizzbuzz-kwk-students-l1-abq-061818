156a9d46c3e78d52514fd91179cffa63f7e1df02
meeraebeida <meeraebeida3@gmail.com>
2018-06-19 20:07:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
 
 def fizzbuzz(number)
-  if number%3==0
+  if number%3==
+    put "Fizz"
   elsif number%5==
     put "Buzz"
-    elsif number
-
+  elsif number%15==
+    put "FizzBuzz"
   end
 end
+sss
